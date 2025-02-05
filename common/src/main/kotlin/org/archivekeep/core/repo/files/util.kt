951a5a24c8066a04dea11bef2c016d7e616c5c7f
Miroslav Kravec <kravec.miroslav@gmail.com>
2025-02-05 08:25:46 +0100

    Fix computeChecksum on large files - do not load whole file to memory

--- common/src/main/kotlin/org/archivekeep/core/repo/files/util.kt
+++ common/src/main/kotlin/org/archivekeep/core/repo/files/util.kt
@@ -4,11 +4,8 @@ import java.nio.file.Path
 import java.nio.file.Paths
 import java.security.MessageDigest
 import kotlin.io.path.invariantSeparatorsPathString
-import kotlin.io.path.readBytes
 
-internal fun safeSubPath(path: String): Path {
-    return safeSubPath(Paths.get(path))
-}
+internal fun safeSubPath(path: String): Path = safeSubPath(Paths.get(path))
 
 internal fun safeSubPath(path: Path): Path {
     val p = path.normalize()
@@ -28,8 +25,11 @@ internal fun safeSubPath(path: Path): Path {
 internal fun computeChecksum(path: Path): String {
     val md = MessageDigest.getInstance("SHA-256")
 
-    val content = path.readBytes()
-    val digest = md.digest(content)
+    path.toFile().forEachBlock(32 * 1024) { buffer, bytesRead ->
+        md.update(buffer, 0, bytesRead)
+    }
+
+    val digest = md.digest()
 
     return digest.joinToString("") { "%02x".format(it) }
 }
