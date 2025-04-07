7c8cc07b456d9eae646f486e5713eeabfe35066e
Miroslav Kravec <kravec.miroslav@gmail.com>
2025-04-07 11:19:23 +0200

    fix: startup of Proguard optimized desktop app crashing with kotlinx.serialization.SerializationException: Serializer for class 'Credentials' is not found

--- modules/app-desktop/build.gradle.kts
+++ modules/app-desktop/build.gradle.kts
@@ -4,6 +4,7 @@ import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization)
 
     alias(libs.plugins.compose)
 
