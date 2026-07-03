757a8ed8586e63a645abc7297b02584353663e99
Miroslav Kravec <kravec.miroslav@gmail.com>
2026-07-03 17:56:13 +0200

    fix: operation log with too many lines crashes UI and application (applies to Add and Push/Pull dialogs)

--- modules/app-ui/src/commonMain/kotlin/org/archivekeep/app/ui/components/designsystem/dialog/LabelText.kt
+++ modules/app-ui/src/commonMain/kotlin/org/archivekeep/app/ui/components/designsystem/dialog/LabelText.kt
@@ -3,10 +3,13 @@ package org.archivekeep.app.ui.components.designsystem.dialog
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
+val labelTextStyle = TextStyle(fontSize = 11.sp, fontWeight = FontWeight.Bold, color = Color.DarkGray)
+
 @Composable
 fun LabelText(text: String) {
-    Text(text, fontSize = 11.sp, fontWeight = FontWeight.Bold, color = Color.DarkGray)
+    Text(text, style = labelTextStyle)
 }
--- modules/app-ui/src/commonMain/kotlin/org/archivekeep/app/ui/components/feature/operations/ScrollableLogText.kt
+++ modules/app-ui/src/commonMain/kotlin/org/archivekeep/app/ui/components/feature/operations/ScrollableLogText.kt
@@ -1,51 +1,80 @@
 package org.archivekeep.app.ui.components.feature.operations
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.rememberTextMeasurer
 import androidx.compose.ui.unit.dp
-import org.archivekeep.app.ui.components.base.layout.ScrollableColumn
+import org.archivekeep.app.ui.components.base.layout.IntrinsicSizeWrapperLayout
+import org.archivekeep.app.ui.components.base.layout.ScrollableLazyColumn
 import org.archivekeep.app.ui.components.designsystem.dialog.LabelText
+import org.archivekeep.app.ui.components.designsystem.dialog.labelTextStyle
+import org.archivekeep.utils.collections.limitSize
+import kotlin.math.max
 
+private const val itemsToConsider = 500
+private const val itemsToMeasure = 30
+
+@Deprecated(
+    "Use overload accepting a list of text lines. This is more expensive, because splits text into individual lines.",
+    ReplaceWith("ScrollableLogTextInDialog(textLines)"),
+)
 @Composable
-fun ScrollableLogTextInDialog(
-    text: String,
-    modifier: Modifier =
-        Modifier
-            .heightIn(min = 40.dp, max = 140.dp)
-            .height(140.dp)
-            .fillMaxWidth(),
-) {
-    ScrollableLogText(text, modifier)
+fun ScrollableLogTextInDialog(text: String) {
+    val textLines = remember(text) { text.split("\n") }
+
+    ScrollableLogTextInDialog(textLines)
 }
 
 @Composable
-fun ScrollableLogText(
-    text: String,
-    modifier: Modifier = Modifier.fillMaxWidth(),
-) {
+fun ScrollableLogTextInDialog(textLines: List<String>) {
+    val textMeasurer = rememberTextMeasurer()
+    val textStyle = MaterialTheme.typography.bodyMedium
+
+    val width =
+        remember(textLines, textStyle) {
+            max(
+                textMeasurer.measure("Log", style = labelTextStyle).size.width,
+                textLines
+                    .limitSize(itemsToConsider)
+                    .map { it }
+                    .sortedByDescending { it.length }
+                    .limitSize(itemsToMeasure)
+                    .maxOfOrNull { textMeasurer.measure(it, style = textStyle).size.width }
+                    ?: 0,
+            )
+        }
+
+    val guessedWidth = with(LocalDensity.current) { width.toDp() } + 60.dp
+
     Surface(
-        modifier = modifier,
+        modifier = Modifier.fillMaxWidth(),
         shape = MaterialTheme.shapes.medium,
         color = Color.Black.copy(0.05f),
     ) {
-        Column(Modifier.fillMaxWidth().padding(start = 10.dp, end = 10.dp, top = 6.dp, bottom = 10.dp)) {
-            LabelText("Log")
-            Spacer(Modifier.height(4.dp))
+        SelectionContainer(Modifier.padding(start = 10.dp, end = 10.dp, top = 6.dp, bottom = 10.dp)) {
+            IntrinsicSizeWrapperLayout(
+                minIntrinsicWidth = guessedWidth,
+                maxIntrinsicWidth = guessedWidth,
+            ) {
+                ScrollableLazyColumn {
+                    item { LabelText("Log") }
+                    item { Spacer(Modifier.height(4.dp)) }
 
-            ScrollableColumn(Modifier.fillMaxWidth()) {
-                SelectionContainer {
-                    Text(text.trimEnd(), Modifier.fillMaxWidth())
+                    items(textLines) { text ->
+                        Text(text.trimEnd())
+                    }
                 }
             }
         }
